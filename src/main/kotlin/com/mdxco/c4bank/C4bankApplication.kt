a36efbcfa998db843a714e6c81c4c6dce44e007d
mendexco <vitormendesco.js@gmail.com>
2024-10-09 23:14:18 -0300

    generating spring project

---
+++ src/main/kotlin/com/mdxco/c4bank/C4bankApplication.kt
@@ -0,0 +1,11 @@
+package com.mdxco.c4bank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class C4bankApplication
+
+fun main(args: Array<String>) {
+	runApplication<C4bankApplication>(*args)
+}
